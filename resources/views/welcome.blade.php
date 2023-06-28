8b82f39025ddf5356bc3d6f74a7f5c779bbfe8bc
Hildafila <filasifahhilda@gmail.com>
2023-06-28 10:25:10 +0700

    Tugas Akhir

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <h1><center> THIS IS MY PROJECT</center></h1>
+        <style>
+            body {
+                background-color:Gray;
+            }
+        </style>
+    </head>
+</html>
